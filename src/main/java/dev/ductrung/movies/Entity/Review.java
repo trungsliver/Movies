9ecf49ff277647b2c15dedfaaa81d4a4015d33db
trungsliver <buiductrung.cva@gmail.com>
2025-09-22 14:27:04 +0700

    entity

---
+++ src/main/java/dev/ductrung/movies/Entity/Review.java
@@ -0,0 +1,20 @@
+package dev.ductrung.movies.Entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collation = "reviews")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Review {
+    @Id
+    private ObjectId id;
+    private String body;
+}
