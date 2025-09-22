8edd8c7dcad7192ce00b19517eeae7031a51ed96
trungsliver <buiductrung.cva@gmail.com>
2025-09-22 15:25:34 +0700

    get movies by Imbd ID

--- src/main/java/dev/ductrung/movies/service/MovieService.java
+++ src/main/java/dev/ductrung/movies/service/MovieService.java
@@ -1,11 +1,13 @@
 package dev.ductrung.movies.service;
 
 import dev.ductrung.movies.repository.MovieRepository;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import dev.ductrung.movies.entity.Movie;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MovieService {
@@ -14,5 +16,9 @@ public class MovieService {
 
     public List<Movie> allMovies() {
         return movieRepository.findAll();
+    }
+
+    public Optional<Movie> getMovieById(String id) {
+        return movieRepository.findMovieByImdbId(id);
     }
 }
